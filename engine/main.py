37b5d4d7308e866197eeec04ae775d04c1c851d2
LBuyana <lathithabuyana38@gmail.com>
2025-11-07 11:24:30 +0200

    Test: Trigger engine deployment

--- engine/main.py
+++ engine/main.py
@@ -8,7 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from typing import List, Dict
 import asyncio
 
-# Load environment variables from .env file: current dir
+# Load environment variables from .env file: current dir: new
 load_dotenv()
 
 # Initialize FastAPI app
